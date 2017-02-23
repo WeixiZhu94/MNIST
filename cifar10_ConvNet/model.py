4321dbb4e5bd5974d6e4aac0fab9f8d9f5299995
Weixi Zhu <Weixi@WeixideMacBook-Pro.local>
2017-02-23 00:35:41 -0600

    0:35

--- cifar10_ConvNet/model.py
+++ cifar10_ConvNet/model.py
@@ -10,22 +10,18 @@ TEST_FILE = 'test.tfrecords'
 def network(images, labels):
 
    net = slim.layers.conv2d(images, 16, [3,3], scope='conv_0_1', normalizer_fn=slim.layers.batch_norm)
-
-   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_1', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_2', normalizer_fn=slim.layers.batch_norm)
+   
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_0_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [5,5], scope='conv_1_1', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_1')
 
    net = slim.layers.conv2d(net, 64, [3,3], scope='conv_2_1', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.conv2d(net, 64, [3,3], scope='conv_2_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 64, [5,5], scope='conv_2_2', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_2')
 
-   net = slim.layers.conv2d(net, 128, [3,3], scope='conv_3_1', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.conv2d(net, 128, [3,3], scope='conv_3_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 256, [3,3], scope='conv_3_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 256, [5,5], scope='conv_3_2', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_3')
-
-   net = slim.layers.conv2d(net, 256, [3,3], scope='conv_4_1', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.conv2d(net, 256, [3,3], scope='conv_4_2', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.max_pool2d(net, [2,2], scope='pool_4')
 
    net = slim.layers.flatten(net, scope='flatten3')
    net = slim.layers.fully_connected(net, 512, scope='fully_connected_1')
