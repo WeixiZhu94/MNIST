99ae0dcfd2392e445d2babd05a828f4c679ac624
Weixi Zhu <Weixi@WeixideMacBook-Pro.local>
2017-02-22 21:50:24 -0600

    21:50

--- cifar10_ConvNet/model.py
+++ cifar10_ConvNet/model.py
@@ -10,7 +10,7 @@ TEST_FILE = 'test.tfrecords'
 def network(images, labels):
 
    net = slim.layers.conv2d(images, 8, [3,3], scope='conv_0_1', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.conv2d(images, 16, [3,3], scope='conv_0_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_0_2', normalizer_fn=slim.layers.batch_norm)
 
    net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_1', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_2', normalizer_fn=slim.layers.batch_norm)
