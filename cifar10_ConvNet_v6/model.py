e766b7052978a969c175e94e5c5594fdc480eb55
Weixi Zhu <Weixi@staff-74-dun20-209.rice.edu>
2017-02-25 17:49:23 -0600

    17:49

--- cifar10_ConvNet_v6/model.py
+++ cifar10_ConvNet_v6/model.py
@@ -7,24 +7,48 @@ TRAIN_FILE = 'train.tfrecords'
 VALIDATION_FILE = 'train.tfrecords'
 TEST_FILE = 'test.tfrecords'
 
+def attention(images):
+   net = slim.layers.conv2d(images, 32, [3,3], scope='att_conv_0_0', normalizer_fn=slim.layers.batch_norm)
+
+   net = slim.layers.conv2d(net, 32, [3,3], scope='att_conv_1_0', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 32, [3,3], scope='att_conv_1_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.max_pool2d(net, [2,2], scope='att_pool_1')
+
+   net = slim.layers.conv2d(net, 128, [3,3], scope='att_conv_2_0', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 128, [3,3], scope='att_conv_2_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.max_pool2d(net, [2,2], scope='att_pool_2')
+
+   net = slim.layers.conv2d(net, 512, [3,3], scope='att_conv_3_0', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 512, [3,3], scope='att_conv_3_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.max_pool2d(net, [2,2], scope='att_pool_3')
 def network(images, labels):
 
-   net = slim.layers.conv2d(images, 32, [3,3], scope='conv_0_0', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(images, 16, [3,3], scope='conv_0_0', normalizer_fn=slim.layers.batch_norm)
    
-   net = slim.layers.conv2d(net, 32, [3,3], scope='conv_0_1', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.conv2d(net, 32, [3,3], scope='conv_1_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_0', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_3', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_4', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_5', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_6', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_1_7', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_1')
 
-   net = slim.layers.conv2d(net, 128, [3,3], scope='conv_2_1', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.conv2d(net, 128, [3,3], scope='conv_2_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 64, [3,3], scope='conv_2_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 64, [1,1], scope='conv_2_2', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_2')
 
-   net = slim.layers.conv2d(net, 512, [3,3], scope='conv_3_1', normalizer_fn=slim.layers.batch_norm)
-   net = slim.layers.conv2d(net, 512, [3,3], scope='conv_3_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 256, [3,3], scope='conv_3_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 256, [1,1], scope='conv_3_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.max_pool2d(net, [2,2], scope='pool_3')
+
+   net = slim.layers.conv2d(net, 1024, [3,3], scope='conv_3_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 1024, [1,1], scope='conv_3_2', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_3')
 
    net = slim.layers.flatten(net, scope='flatten')
-   net = slim.layers.fully_connected(net, 512, scope='fully_connected_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.fully_connected(net, 1024, scope='fully_connected_1', normalizer_fn=slim.layers.batch_norm)
    logits = slim.layers.fully_connected(net, 10, activation_fn=None, scope='logits', normalizer_fn=slim.layers.batch_norm)
    
    slim.losses.sparse_softmax_cross_entropy(logits, labels)
