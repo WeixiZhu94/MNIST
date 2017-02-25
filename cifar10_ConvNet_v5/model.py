65a7d8c86cdc26c50308b1216d67419d33b06bf1
Weixi Zhu <Weixi@WeixideMacBook-Pro.local>
2017-02-24 22:24:28 -0600

    22:24

--- cifar10_ConvNet_v5/model.py
+++ cifar10_ConvNet_v5/model.py
@@ -9,21 +9,21 @@ TEST_FILE = 'test.tfrecords'
 
 def network(images, labels):
 
-   net = slim.layers.conv2d(images, 8, [3,3], scope='conv_0_0', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(images, 16, [3,3], scope='conv_0_0', normalizer_fn=slim.layers.batch_norm)
    
-   net = slim.layers.conv2d(net, 16, [3,3], scope='conv_0_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 64, [3,3], scope='conv_0_1', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.conv2d(net, 64, [3,3], scope='conv_1_1', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_1')
 
-   net = slim.layers.conv2d(net, 64, [3,3], scope='conv_2_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 256, [3,3], scope='conv_2_1', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.conv2d(net, 256, [3,3], scope='conv_2_2', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_2')
 
-   net = slim.layers.conv2d(net, 256, [3,3], scope='conv_3_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 1024, [3,3], scope='conv_3_1', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.conv2d(net, 1024, [3,3], scope='conv_3_2', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.max_pool2d(net, [2,2], scope='pool_3')
 
-   net = slim.layers.flatten(net, scope='flatten3')
+   net = slim.layers.flatten(net, scope='flatten')
    net = slim.layers.fully_connected(net, 1024, scope='fully_connected_1', normalizer_fn=slim.layers.batch_norm)
    net = slim.layers.fully_connected(net, 1024, scope='fully_connected_2', normalizer_fn=slim.layers.batch_norm)
    logits = slim.layers.fully_connected(net, 10, activation_fn=None, scope='logits')
