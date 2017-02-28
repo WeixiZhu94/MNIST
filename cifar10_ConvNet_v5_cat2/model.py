79f1e56fe62c1ca7d29bc78416c9ec0164a97b53
Weixi Zhu <Weixi@WeixideMacBook-Pro.local>
2017-02-28 00:27:48 -0600

    0:27

---
+++ cifar10_ConvNet_v5_cat2/model.py
@@ -0,0 +1,42 @@
+import os
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim\
+
+TRAIN_FILE = 'train.tfrecords'
+VALIDATION_FILE = 'train.tfrecords'
+TEST_FILE = 'test.tfrecords'
+
+def _cat2(labels):
+   table1 = tf.constant([1,1,0,0,0,0,0,0,1,1])
+   table2 = 1 - table1
+   A = tf.stack([table1, table2], axis=0)
+   one_hot = tf.one_hot(labels, 10, 1, 0, axis=-1)
+   labels_cat2 = tf.matmul(one_hot, A)
+   labels_cat2 = tf.argmax(labels_cat2, axis=1)
+
+def network(images, labels):
+
+   net = slim.layers.conv2d(images, 16, [3,3], scope='conv_0_0', normalizer_fn=slim.layers.batch_norm)
+   
+   net = slim.layers.conv2d(net, 64, [3,3], scope='conv_0_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 64, [3,3], scope='conv_1_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.max_pool2d(net, [2,2], scope='pool_1')
+
+   net = slim.layers.conv2d(net, 256, [3,3], scope='conv_2_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 256, [3,3], scope='conv_2_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.max_pool2d(net, [2,2], scope='pool_2')
+
+   net = slim.layers.conv2d(net, 1024, [3,3], scope='conv_3_1', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.conv2d(net, 1024, [3,3], scope='conv_3_2', normalizer_fn=slim.layers.batch_norm)
+   net = slim.layers.max_pool2d(net, [2,2], scope='pool_3')
+
+   net = slim.layers.flatten(net, scope='flatten')
+   net = slim.layers.fully_connected(net, 1024, scope='fully_connected_1', normalizer_fn=slim.layers.batch_norm)
+   #net = slim.layers.fully_connected(net, 1024, scope='fully_connected_2', normalizer_fn=slim.layers.batch_norm)
+   logits = slim.layers.fully_connected(net, 2, activation_fn=None, scope='logits')
+   
+   labels_cat2 = _cat2(labels)
+   slim.losses.sparse_softmax_cross_entropy(logits, labels_cat2)
+   total_loss = slim.losses.get_total_loss()
+   return logits, total_loss, labels_cat2
---
+++ cifar10_ConvNet_v5_cat2/train.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from model import network, _cat2
+from cifar_input import build_input
+
+flags = tf.app.flags
+flags.DEFINE_string('train_dir', '../data',
+                    'Directory with the training data.')
+flags.DEFINE_integer('batch_size', 100, 'Batch size.')
+flags.DEFINE_integer('num_batches', None, 'Num of batches to train (epochs).')
+flags.DEFINE_string('log_dir', '../log/cifar10_ConvNet_v5_cat2/train',
+                    'Directory with the log data.')
+FLAGS = flags.FLAGS
+
+
+def main(train_dir, batch_size, num_batches, log_dir):
+
+    images, labels = build_input('cifar10', 100, 'train')
+    predictions, total_loss, labels_cat2 = network(images, labels)
+    tf.summary.scalar('loss', total_loss)
+
+    predictions = tf.to_int32(tf.argmax(predictions, 1))
+    tf.summary.scalar('accuracy_cat2', slim.metrics.accuracy(predictions, labels_cat2))
+
+    optimizer = tf.train.GradientDescentOptimizer(0.1)
+    train_op = slim.learning.create_train_op(total_loss, optimizer, summarize_gradients=True)
+
+    slim.learning.train(train_op, log_dir, save_summaries_secs=20, save_interval_secs=20)
+
+
+if __name__ == '__main__':
+    main(FLAGS.train_dir,
+         FLAGS.batch_size,
+         FLAGS.num_batches,
+         FLAGS.log_dir)
