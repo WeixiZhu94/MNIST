24103e7a9cc6de58aff6cfe8d56ce09d635415f2
Weixi Zhu <Weixi@staff-74-dun20-131.rice.edu>
2017-02-28 13:56:07 -0600

    13:56

---
+++ cifar10_AsymNet/model.py
@@ -0,0 +1,59 @@
+import os
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim\
+
+TRAIN_FILE = 'train.tfrecords'
+VALIDATION_FILE = 'train.tfrecords'
+TEST_FILE = 'test.tfrecords'
+
+def network(images, labels):
+
+   net_1 = slim.layers.conv2d(net, 8, [1,1], scope='init_conv_1', normalizer_fn=slim.layers.batch_norm)
+   net_2 = slim.layers.conv2d(net, 4, [3,3], scope='init_conv_2', normalizer_fn=slim.layers.batch_norm)
+   net_3 = slim.layers.conv2d(net, 2, [5,5], scope='init_conv_3', normalizer_fn=slim.layers.batch_norm)
+   net_4 = slim.layers.conv2d(net, 2, [7,7], scope='init_conv_4', normalizer_fn=slim.layers.batch_norm)
+   net = tf.concat([net_1, net_2, net_3, net_4], 3)
+
+   net_1 = slim.layers.conv2d(net, 8, [1,1], scope='conv_1_1_1', normalizer_fn=slim.layers.batch_norm)
+   net_2 = slim.layers.conv2d(net, 4, [3,3], scope='conv_1_1_2', normalizer_fn=slim.layers.batch_norm)
+   net_3 = slim.layers.conv2d(net, 2, [5,5], scope='conv_1_1_3', normalizer_fn=slim.layers.batch_norm)
+   net_4 = slim.layers.conv2d(net, 2, [7,7], scope='conv_1_1_4', normalizer_fn=slim.layers.batch_norm)
+   net = tf.concat([net_1, net_2, net_3, net_4], 3)
+   net_1 = slim.layers.conv2d(net, 8, [1,1], scope='conv_1_2_1', normalizer_fn=slim.layers.batch_norm)
+   net_2 = slim.layers.conv2d(net, 4, [3,3], scope='conv_1_2_2', normalizer_fn=slim.layers.batch_norm)
+   net_3 = slim.layers.conv2d(net, 2, [5,5], scope='conv_1_2_3', normalizer_fn=slim.layers.batch_norm)
+   net_4 = slim.layers.conv2d(net, 2, [7,7], scope='conv_1_2_4', normalizer_fn=slim.layers.batch_norm)
+   net = tf.concat([net_1, net_2, net_3, net_4], 3)
+   net = slim.layers.max_pool2d(net, [2,2], scope='pool_1')
+
+   net_1 = slim.layers.conv2d(net, 16, [1,1], scope='conv_2_1_1', normalizer_fn=slim.layers.batch_norm)
+   net_2 = slim.layers.conv2d(net, 9, [3,3], scope='conv_2_1_2', normalizer_fn=slim.layers.batch_norm)
+   net_3 = slim.layers.conv2d(net, 5, [5,5], scope='conv_2_1_3', normalizer_fn=slim.layers.batch_norm)
+   net_4 = slim.layers.conv2d(net, 2, [7,7], scope='conv_2_1_4', normalizer_fn=slim.layers.batch_norm)
+   net = tf.concat([net_1, net_2, net_3, net_4], 3)
+   net_1 = slim.layers.conv2d(net, 16, [1,1], scope='conv_2_2_1', normalizer_fn=slim.layers.batch_norm)
+   net_2 = slim.layers.conv2d(net, 9, [3,3], scope='conv_2_2_2', normalizer_fn=slim.layers.batch_norm)
+   net_3 = slim.layers.conv2d(net, 5, [5,5], scope='conv_2_2_3', normalizer_fn=slim.layers.batch_norm)
+   net_4 = slim.layers.conv2d(net, 2, [7,7], scope='conv_2_2_4', normalizer_fn=slim.layers.batch_norm)
+   net = tf.concat([net_1, net_2, net_3, net_4], 3)
+   net = slim.layers.max_pool2d(net, [2,2], scope='pool_2')
+
+   net_1 = slim.layers.conv2d(net, 33, [1,1], scope='conv_3_1_1', normalizer_fn=slim.layers.batch_norm)
+   net_2 = slim.layers.conv2d(net, 17, [3,3], scope='conv_3_1_2', normalizer_fn=slim.layers.batch_norm)
+   net_3 = slim.layers.conv2d(net, 9, [5,5], scope='conv_3_1_3', normalizer_fn=slim.layers.batch_norm)
+   net_4 = slim.layers.conv2d(net, 5, [7,7], scope='conv_3_1_4', normalizer_fn=slim.layers.batch_norm)
+   net = tf.concat([net_1, net_2, net_3, net_4], 3)
+   net_1 = slim.layers.conv2d(net, 33, [1,1], scope='conv_3_2_1', normalizer_fn=slim.layers.batch_norm)
+   net_2 = slim.layers.conv2d(net, 17, [3,3], scope='conv_3_2_2', normalizer_fn=slim.layers.batch_norm)
+   net_3 = slim.layers.conv2d(net, 9, [5,5], scope='conv_3_2_3', normalizer_fn=slim.layers.batch_norm)
+   net_4 = slim.layers.conv2d(net, 5, [7,7], scope='conv_3_2_4', normalizer_fn=slim.layers.batch_norm)
+   net = tf.concat([net_1, net_2, net_3, net_4], 3)
+   net = slim.layers.max_pool2d(net, [2,2], scope='pool_3')
+
+   net = tf.reduce_mean(net, [1,2])
+   logits = slim.layers.fully_connected(net, 10, activation_fn=None, scope='logits', normalizer_fn=slim.layers.batch_norm)
+   
+   slim.losses.sparse_softmax_cross_entropy(logits, labels)
+   total_loss = slim.losses.get_total_loss()
+   return logits, total_loss
