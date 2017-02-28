0a85ec0cbf771295c03473a785442db6cf3635c1
Weixi Zhu <Weixi@WeixideMacBook-Pro.local>
2017-02-28 01:07:29 -0600

    1:07

--- cifar10_ConvNet_v5_cat2/model.py
+++ cifar10_ConvNet_v5_cat2/model.py
@@ -8,15 +8,11 @@ VALIDATION_FILE = 'train.tfrecords'
 TEST_FILE = 'test.tfrecords'
 
 def _cat2(labels):
-   #table1 = tf.constant([1,1,0,0,0,0,0,0,1,1])
-   #table2 = tf.constant([0,0,1,1,1,1,1,1,0,0])
-   #A = tf.transpose(tf.stack([table1, table2], axis=0))
-   one_hot = tf.one_hot(labels, 10, 1.0, 0.0, axis=-1)
-   cat_1_sparse = tf.SparseTensor(indices = [[0,1], [1,1], [2,0], [3,0], [4,0], [5,0], [6,0], [7,0], [8,1], [9,1]], values = [1,1,1,1,1,1,1,1,1,1], dense_shape = [10, 2])
-   value = tf.sparse_tensor_to_dense(cat_1_sparse)
-   value = tf.cast(value, tf.float32)
-   w = tf.get_variable('L1', initializer=value, trainable=False)
-   labels_cat2 = tf.matmul(one_hot, w)
+   table1 = tf.constant([1,1,0,0,0,0,0,0,1,1])
+   table2 = tf.constant([0,0,1,1,1,1,1,1,0,0])
+   A = tf.transpose(tf.stack([table1, table2], axis=0))
+   one_hot = tf.one_hot(labels, 10, 1, 0, axis=-1)
+   return tf.argmax(tf.matmul(one_hot, A), axis=1)
 
 def network(images, labels):
 
@@ -40,5 +36,5 @@ def network(images, labels):
    logits = slim.layers.fully_connected(net, 2, activation_fn=None, scope='logits')
    
    labels_cat2 = _cat2(labels)
-   total_loss = tf.losses.softmax_cross_entropy(logits, labels_cat2)
+   total_loss = tf.losses.sparse_softmax_cross_entropy(logits, labels_cat2)
    return logits, total_loss, labels_cat2
