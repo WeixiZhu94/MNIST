0f457e9358c2448d694794004c72c4014525ec23
Weixi Zhu <Weixi@staff-74-dun20-131.rice.edu>
2017-02-28 19:20:01 -0600

    ResNet

--- ResNet_cat2/train.py
+++ ResNet_cat2/train.py
@@ -9,7 +9,7 @@ flags.DEFINE_string('train_dir', '../data',
                     'Directory with the training data.')
 flags.DEFINE_integer('batch_size', 100, 'Batch size.')
 flags.DEFINE_integer('num_batches', None, 'Num of batches to train (epochs).')
-flags.DEFINE_string('log_dir', '../log/ResNet/train',
+flags.DEFINE_string('log_dir', '../log/ResNet_cat2/train',
                     'Directory with the log data.')
 FLAGS = flags.FLAGS
 
