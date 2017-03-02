15969c5069903d3d4fd86fd59e2e11e6e46adf53
Weixi Zhu <Weixi@WeixideMacBook-Pro.local>
2017-03-02 13:49:32 -0600

    Emsemble

--- EmsResNet_cat_0/eval.py
+++ EmsResNet_cat_0/eval.py
@@ -10,9 +10,9 @@ flags.DEFINE_string('train_dir', '../data',
                     'Directory with the training data.')
 flags.DEFINE_integer('batch_size', 100, 'Batch size.')
 flags.DEFINE_integer('num_batches', 100, 'Num of batches to evaluate.')
-flags.DEFINE_string('log_dir', '../log/EmsResNet/eval',
+flags.DEFINE_string('log_dir', '../log/EmsResNet_cat_0/eval',
                     'Directory where to log data.')
-flags.DEFINE_string('checkpoint_dir', '../log/EmsResNet/train',
+flags.DEFINE_string('checkpoint_dir', '../log/EmsResNet_cat_0/train',
                     'Directory with the model checkpoint data.')
 FLAGS = flags.FLAGS
 
@@ -22,28 +22,28 @@ def main(train_dir, batch_size, num_batches, log_dir, checkpoint_dir=None):
         checkpoint_dir = log_dir
     with tf.device('/cpu:0'):
       images, labels, images_cat_0, labels_cat_0, images_cat_1, labels_cat_1 = build_input('cifar10', 100, 'test')
-      predictions, total_loss, labels_cat2 = network(images, labels)
+      #predictions, total_loss, labels_cat2 = network(images, labels)
       predictions_cat_0, loss_0 = cat_0_network(images_cat_0, labels_cat_0)
-      predictions_cat_1, loss_1 = cat_1_network(images_cat_1, labels_cat_1)
+      #predictions_cat_1, loss_1 = cat_1_network(images_cat_1, labels_cat_1)
     
-      tf.summary.scalar('loss', total_loss)
+      #tf.summary.scalar('loss', total_loss)
       tf.summary.scalar('loss_0', loss_0)
-      tf.summary.scalar('loss_1', loss_1)
+      #tf.summary.scalar('loss_1', loss_1)
     
 
       predictions_cat_0 = tf.argmax(predictions_cat_0, axis=1)
-      predictions_cat_1 = tf.argmax(predictions_cat_1, axis=1)
-      predictions = tf.argmax(predictions, 1)
+      #predictions_cat_1 = tf.argmax(predictions_cat_1, axis=1)
+      #predictions = tf.argmax(predictions, 1)
       tf.summary.scalar('accuracy_cat_0', slim.metrics.accuracy(predictions_cat_0, tf.to_int64(labels_cat_0)))
-      tf.summary.scalar('accuracy_cat_1', slim.metrics.accuracy(predictions_cat_1, tf.to_int64(labels_cat_1)))
-      tf.summary.scalar('accuracy_cat2', slim.metrics.accuracy(predictions, labels_cat2))
+      #tf.summary.scalar('accuracy_cat_1', slim.metrics.accuracy(predictions_cat_1, tf.to_int64(labels_cat_1)))
+      #tf.summary.scalar('accuracy_cat2', slim.metrics.accuracy(predictions, labels_cat2))
 
       # These are streaming metrics which compute the "running" metric,
       # e.g running accuracy
       metrics_to_values, metrics_to_updates = slim.metrics.aggregate_metric_map({
-          'accuracy_cat2': slim.metrics.streaming_accuracy(predictions, labels_cat2),
+          #'accuracy_cat2': slim.metrics.streaming_accuracy(predictions, labels_cat2),
           'accuracy_cat_0': slim.metrics.streaming_accuracy(predictions_cat_0, labels_cat_0),
-          'accuracy_cat_1': slim.metrics.streaming_accuracy(predictions_cat_1, labels_cat_1),
+          #'accuracy_cat_1': slim.metrics.streaming_accuracy(predictions_cat_1, labels_cat_1),
       })
 
       # Define the streaming summaries to write:
---
+++ EmsResNet_cat_0/train.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import sys
+from model import network, cat_0_network, cat_1_network
+from cifar_input import build_input
+
+flags = tf.app.flags
+flags.DEFINE_string('train_dir', '../data',
+                    'Directory with the training data.')
+flags.DEFINE_integer('batch_size', 100, 'Batch size.')
+flags.DEFINE_integer('num_batches', None, 'Num of batches to train (epochs).')
+flags.DEFINE_string('log_dir', '../log/EmsResNet_cat_0/train',
+                    'Directory with the log data.')
+FLAGS = flags.FLAGS
+
+def report():
+    param_stats = tf.contrib.tfprof.model_analyzer.print_model_analysis(
+        tf.get_default_graph(),
+        tfprof_options=tf.contrib.tfprof.model_analyzer.
+            TRAINABLE_VARS_PARAMS_STAT_OPTIONS)
+    sys.stdout.write('total_params: %d\n' % param_stats.total_parameters)
+
+    tf.contrib.tfprof.model_analyzer.print_model_analysis(
+        tf.get_default_graph(),
+        tfprof_options=tf.contrib.tfprof.model_analyzer.FLOAT_OPS_OPTIONS)
+
+def main(train_dir, batch_size, num_batches, log_dir):
+
+    images, labels, images_cat_0, labels_cat_0, images_cat_1, labels_cat_1 = build_input('cifar10', 100, 'train')
+    #predictions, total_loss, labels_cat2 = network(images, labels)
+    predictions_cat_0, loss_0 = cat_0_network(images_cat_0, labels_cat_0)
+    #predictions_cat_1, loss_1 = cat_1_network(images_cat_1, labels_cat_1)
+    
+    report()
+
+    #tf.summary.scalar('loss', total_loss)
+    tf.summary.scalar('loss_0', loss_0)
+    #tf.summary.scalar('loss_1', loss_1)
+    #predictions = tf.argmax(predictions, axis=1)
+    #tf.summary.scalar('accuracy', slim.metrics.accuracy(predictions, tf.to_int64(labels_cat2)))
+
+    predictions_cat_0 = tf.argmax(predictions_cat_0, axis=1)
+    #predictions_cat_1 = tf.argmax(predictions_cat_1, axis=1)
+    tf.summary.scalar('accuracy_cat_0', slim.metrics.accuracy(predictions_cat_0, tf.to_int64(labels_cat_0)))
+    #tf.summary.scalar('accuracy_cat_1', slim.metrics.accuracy(predictions_cat_1, tf.to_int64(labels_cat_1)))
+
+
+    optimizer = tf.train.GradientDescentOptimizer(0.1)
+    #train_op = slim.learning.create_train_op(total_loss, optimizer, summarize_gradients=True)
+    train_op_0 = slim.learning.create_train_op(loss_0, optimizer, summarize_gradients=True)
+    #train_op_2 = slim.learning.create_train_op(loss_1, optimizer, summarize_gradients=True)
+
+    slim.learning.train(train_op_0, log_dir, save_summaries_secs=20, save_interval_secs=20)
+
+
+if __name__ == '__main__':
+    main(FLAGS.train_dir,
+         FLAGS.batch_size,
+         FLAGS.num_batches,
+         FLAGS.log_dir)
