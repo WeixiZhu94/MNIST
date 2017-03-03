9f04cf5ba1aa915d74a6a310d3e77a1fbe1d459f
Weixi Zhu <Weixi@WeixideMacBook-Pro.local>
2017-03-03 00:19:14 -0600

    Ems

--- EmsResNet_vali/train.py
+++ EmsResNet_vali/train.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 import sys
-from model import network, cat_0_network, cat_1_network
-from cifar_input import build_input, build_input_cat_0, build_input_cat_1
+from model import network
+from cifar_input import build_input
 
 flags = tf.app.flags
 flags.DEFINE_string('train_dir', '../data',
                     'Directory with the training data.')
 flags.DEFINE_integer('batch_size', 100, 'Batch size.')
 flags.DEFINE_integer('num_batches', None, 'Num of batches to train (epochs).')
-flags.DEFINE_string('log_dir', '../log/EmsResNet_cat_1/train',
+flags.DEFINE_string('log_dir', '../log/EmsResNet_vali/train',
                     'Directory with the log data.')
 FLAGS = flags.FLAGS
 
@@ -26,14 +26,14 @@ def report():
 
 def main(train_dir, batch_size, num_batches, log_dir):
 
-    images_cat_1, labels_cat_1 = build_input_cat_1('cifar10', 100, 'test')
+    images, labels = build_input_cat_1('cifar10', 100, 'test')
 
-    predictions_cat_1, loss_1, _ = cat_1_network(images_cat_1, labels_cat_1)
+    predictions, loss_1, _ = network(images, labels)
     
     report()
 
     tf.summary.scalar('loss_1', loss_1)
-    predictions_cat_1 = tf.argmax(predictions_cat_1, axis=1)
+    predictions = tf.argmax(predictions, axis=1)
 
     tf.summary.scalar('accuracy_cat_1', slim.metrics.accuracy(predictions_cat_1, tf.to_int64(labels_cat_1)))
 
