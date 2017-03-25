36a4ffe7989cf77826d935c5c7fd69517736dc2e
Weixi Zhu <Weixi@WeixideMacBook-Pro.local>
2017-03-25 15:31:23 -0500

    ass2

--- RN_sgd/mnist.py
+++ RN_sgd/mnist.py
@@ -63,11 +63,8 @@ def network(net, labels):
 
    logits = slim.layers.fully_connected(net, 10, activation_fn=None, scope='FC_10',biases_regularizer=regularizer, weights_regularizer=regularizer)
  
-   #loss = tf.losses.sparse_softmax_cross_entropy(labels,logits)
-   logits = tf.squeeze(logits)
-   labels = tf.one_hot(labels, 10, 1.0, 0.0, axis=-1)
-   logits = tf.nn.log_softmax(logits)
-   loss = -tf.reduce_sum(logits*labels, axis=0)
+   labels = tf.expand_dims(logits, 0)
+   loss = tf.losses.sparse_softmax_cross_entropy(labels,logits)
    total_loss = loss
    return logits, total_loss
 
